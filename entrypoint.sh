7472451338cb80f8c3b819a39eddd30aaa84be62
Antoine Frau <antoine@korbit.ai>
2023-08-29 23:51:59 +0200

    Fix output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh -l
 
-#!/bin/bash
-
 OUTPUT_PATH=".korbit/scan.log"
 path=$1
 threshold_priority=$2
@@ -20,11 +18,8 @@ if [ "$headless_show_report" = true ]; then
     cmd="$cmd --headless-show-report"
 fi
 
-korbit scan --help
-
 eval $cmd
-ls -la .korbit
-ls -la
 
-echo "$(cat $OUTPUT_PATH)" >> $GITHUB_OUTPUT
+if [ -f $OUTPUT_PATH ]; then
+    $(cat $OUTPUT_PATH) >> $GITHUB_OUTPUT
 
