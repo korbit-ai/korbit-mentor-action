7851d5efcf06b227043732b7e3e1b90bcb0512c6
Antoine Frau <antoine@korbit.ai>
2023-08-29 22:31:35 +0200

    Debug output

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,6 @@ threshold_confidence=$3
 headless=$4
 headless_show_report=$5
 
-echo "Environment variables:"
-echo "KORBIT_SECRET_ID: $KORBIT_SECRET_ID"
-echo "KORBIT_SECRET_KEY: $KORBIT_SECRET_KEY"
-
-echo "Running korbit scan with the following arguments:"
-echo "Path: $path"
-echo "Threshold Priority: $threshold_priority"
-echo "Threshold Confidence: $threshold_confidence"
-echo "Headless: $headless"
-echo "Headless Show Report: $headless_show_report"
-
 cmd="korbit scan $path \
     --threshold-priority=$threshold_priority \
     --threshold-confidence=$threshold_confidence"
@@ -32,14 +21,10 @@ if [ "$headless_show_report" = true ]; then
 fi
 
 korbit scan --help
-python -m korbit scan --help
 
-echo "Executing korbit scan command..."
-echo "Command: $cmd"
 eval $cmd
+ls -la $OUTPUT_PATH
+ls -la
 
-echo "korbit scan command completed."
-echo "\n\nOutput:"
-echo "$(cat $OUTPUT_PATH)"
 echo "$(cat $OUTPUT_PATH)" >> $GITHUB_OUTPUT
 
